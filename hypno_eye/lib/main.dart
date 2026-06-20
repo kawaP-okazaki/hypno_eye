bb54ab2d51f4a91dd549625c4c2e6d43ab30506c
kawaP-okazaki <brave.licence@gmail.com>
2026-06-20 16:01:14 +0900

    first ios build

--- hypno_eye/lib/main.dart
+++ hypno_eye/lib/main.dart
@@ -1,122 +1,180 @@
 import 'package:flutter/material.dart';
+import 'package:vibration/vibration.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const HypnoEyeApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HypnoEyeApp extends StatelessWidget {
+  const HypnoEyeApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
 
-  void _incrementCounter() {
+  bool _isFlashing = false;
+  bool _flashOn = false;
+
+  final Color pinkDark = const Color(0xFFFC0FC0);
+  final Color pinkBright = const Color(0xFFFF5FD6);
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1200),
+    )..repeat(reverse: true);
+  }
+
+  Future<void> _flashEffect() async {
+    if (_isFlashing) return;
+
+    _isFlashing = true;
+
+    for (int i = 0; i < 3; i++) {
+      try {
+        if (await Vibration.hasVibrator()) {
+          Vibration.vibrate(duration: 80);
+        }
+      } catch (_) {}
+
+      setState(() {
+        _flashOn = true;
+      });
+
+      await Future.delayed(
+        const Duration(milliseconds: 100),
+      );
+
+      setState(() {
+        _flashOn = false;
+      });
+
+      await Future.delayed(
+        const Duration(milliseconds: 100),
+      );
+    }
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isFlashing = false;
     });
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Color _backgroundColor() {
+    final t = Curves.easeInOut.transform(
+      _controller.value,
+    );
+
+    return Color.lerp(
+          pinkDark,
+          pinkBright,
+          t,
+        ) ??
+        pinkDark;
+  }
+
+  double _pulseScale() {
+    if (_isFlashing) {
+      return _flashOn ? 1.15 : 1.0;
+    }
+
+    return 0.95 + (_controller.value * 0.10);
+  }
+
+  double _glowOpacity() {
+    if (_isFlashing) {
+      return _flashOn ? 0.90 : 0.35;
+    }
+
+    return 0.20 + (_controller.value * 0.25);
+  }
+
+  double _mainOpacity() {
+    if (_isFlashing) {
+      return _flashOn ? 1.0 : 0.8;
+    }
+
+    return 0.85 + (_controller.value * 0.15);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return GestureDetector(
+          onTap: _flashEffect,
+          child: Scaffold(
+            body: AnimatedContainer(
+              duration: const Duration(milliseconds: 100),
+              width: double.infinity,
+              height: double.infinity,
+              color: _backgroundColor(),
+              child: Center(
+                child: Transform.scale(
+                  scale: _pulseScale(),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Opacity(
+                        opacity: _glowOpacity(),
+                        child: ColorFiltered(
+                          colorFilter: const ColorFilter.mode(
+                            Color(0xFFC27BFF),
+                            BlendMode.srcIn,
+                          ),
+                          child: Image.asset(
+                            'assets/images/inmon.png',
+                            width: 450,
+                          ),
+                        ),
+                      ),
+
+                      Opacity(
+                        opacity: _mainOpacity(),
+                        child: ColorFiltered(
+                          colorFilter: const ColorFilter.mode(
+                            Color(0xFF8D3ED9),
+                            BlendMode.srcIn,
+                          ),
+                          child: Image.asset(
+                            'assets/images/inmon.png',
+                            width: 420,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+          ),
+        );
+      },
     );
   }
 }
